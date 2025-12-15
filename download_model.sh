441ebcc5d75eee054a4d93bac4368d489966a6b1
npdji <npdji@users.noreply.github.com>
2025-12-15 23:48:23 +0000

    cleanup: Remove unnecessary documentation and test files - keep only essential code

--- download_model.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Downloading Stories15M model and tokenizer..."
-wget -O stories15M.bin https://huggingface.co/karpathy/tinyllamas/resolve/main/stories15M.bin
-wget -O tokenizer.bin https://github.com/karpathy/llama2.c/raw/master/tokenizer.bin
-echo "Done! Model and tokenizer downloaded."
